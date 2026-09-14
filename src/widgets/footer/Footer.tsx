3d3ddbf8c80329760306ca7644496029ffbbeca3
Eunmi Lee <eunoia0235@gmail.com>
2026-09-15 02:05:47 +0900

    feat: 전체 레이아웃 컴포넌트 - header, footer 적용

---
+++ src/widgets/footer/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-secondary pt-6 text-center">
+      © 2026 Eunmi Developer Portfolio. All rights reserved.
+    </footer>
+  );
+}
